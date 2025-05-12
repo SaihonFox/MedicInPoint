1b53a67726ff34815d7e09749ba0753a6e2c87d4
SaihonFox <SaihonFox@users.github.com>
2025-05-12 15:53:20 +0500

    ~

--- MedicInPoint/Models/Analysis.cs
+++ MedicInPoint/Models/Analysis.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace MedicInPoint.Models;
+﻿namespace MedicInPoint.Models;
 
 public partial class Analysis
 {
-    public int Id { get; set; }
+	public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+	public string Name { get; set; } = null!;
 
-    public string? Description { get; set; }
+	public string? Description { get; set; }
 
-    public string? Preparation { get; set; }
+	public string? Preparation { get; set; }
 
-    public string ResultsAfter { get; set; } = null!;
+	public string ResultsAfter { get; set; } = null!;
 
-    public string Biomaterial { get; set; } = null!;
+	public string Biomaterial { get; set; } = null!;
 
-    public decimal Price { get; set; }
+	public decimal Price { get; set; }
 
-    public ICollection<AdBlock> AdBlocks { get; set; } = [];
+	public ICollection<AdBlock> AdBlocks { get; set; } = [];
 
-    public ICollection<AnalysisCategoriesList> AnalysisCategoriesLists { get; set; } = [];
+	public ICollection<AnalysisCategoriesList> AnalysisCategoriesLists { get; set; } = [];
 
-    public ICollection<PatientAnalysisCartItem> PatientAnalysisCartItems { get; set; } = [];
+	public ICollection<PatientAnalysisCartItem> PatientAnalysisCartItems { get; set; } = [];
 
-    public ICollection<RequestAnalysis> RequestAnalyses { get; set; } = [];
+	public ICollection<RequestAnalysis> RequestAnalyses { get; set; } = [];
 }
